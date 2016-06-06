ae34d0f7c37664591bbd09f66beabebf692f104a
Daniel Sloane <dsloane@catalystdevworks.com>
2016-06-06 12:12:25 -0700

    get all kudos now searches larger size

--- src/main/java/assessment/services/KudoService.java
+++ src/main/java/assessment/services/KudoService.java
@@ -2,6 +2,7 @@ package assessment.services;
 
 import assessment.entities.KudoApi;
 import assessment.entities.kudo.Kudo;
+import assessment.entities.user.User;
 import assessment.services.interfaces.IKudoApiService;
 import assessment.utilities.UrlConstants;
 import assessment.utilities.httpclient.jsonparser.DataJsonParser;
@@ -10,6 +11,7 @@ import org.apache.http.HttpException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -27,7 +29,7 @@ public class KudoService extends BaseService<KudoApi> implements IKudoApiService
 
         ArrayList<KudoApi> kudos = new ArrayList<KudoApi>();
 
-        List<Kudo> kudoList = client.getAll(url, Kudo.class);
+        List<Kudo> kudoList = client.getAll(url + "?size=3000000", Kudo.class);
 
         for (Kudo kudo : kudoList) {
             KudoApi kudoApi = new KudoApi();
@@ -54,7 +56,7 @@ public class KudoService extends BaseService<KudoApi> implements IKudoApiService
 
         ArrayList<KudoApi> kudos = new ArrayList<KudoApi>();
 
-        List<Kudo> kudoList = client.getAll(url, Kudo.class);
+        List<Kudo> kudoList = client.getAll(url + "?size=3000000", Kudo.class);
 
         for (Kudo kudo : kudoList) {
 
@@ -100,13 +102,31 @@ public class KudoService extends BaseService<KudoApi> implements IKudoApiService
     @Override
     public KudoApi create(KudoApi kudoApi) throws HttpException {
 
-        Kudo kudo = new Kudo(kudoApi.getReviewer().getId(), kudoApi.getReviewedId(), kudoApi.getComment(), kudoApi.getSubmittedDate());
+        System.out.println("in kudoapi create, kudoApi reviewed id = " + kudoApi.getReviewedId());
+        System.out.println("reviewer id = " + kudoApi.getReviewer().getId());
+        System.out.println("comment = " + kudoApi.getComment());
+
+        UserService userService = new UserService();
+        User reviewed = userService.get(kudoApi.getReviewedId());
+        User reviewer = userService.get(kudoApi.getReviewer().getId());
+
+        System.out.println("The person being reviewed = " + reviewed.getFirstName() + " " + reviewed.getLastName());
+        System.out.println("The reviewer = " + reviewer.getFirstName() + " " + reviewer.getLastName());
+
+        Kudo kudo = new Kudo(kudoApi.getReviewer().getId(), kudoApi.getReviewedId(), kudoApi.getComment(), new Date());
         Kudo returnKudo = client.post(url, kudo, Kudo.class);
 
-        if (returnKudo == kudo) {
+        System.out.println("returnKudo = " + returnKudo);
+        System.out.println("returnKudo reviewerId = " + returnKudo.getReviewerId());
+        System.out.println("returnKudo reviewedId = " + returnKudo.getReviewedId());
+        System.out.println("returnKudo  date = " + returnKudo.getSubmittedDate());
+        System.out.println("returnKudo comment = " + returnKudo.getComment());
+
+        return kudoApi;
+      /*  if (returnKudo == kudo) {
             return kudoApi;
         }
-        return null;
+        return null; */
     }
 
     @Override
